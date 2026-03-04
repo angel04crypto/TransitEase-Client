5d06320875b61d7a4683ee034a42da2823f20dc6
angel04crypto <angelrai3034@gmail.com>
2026-03-04 05:42:26 +0530

    Removed localhost fallback from API baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -2,18 +2,28 @@ import axios from 'axios';
 import { auth } from '../firebase/config';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
-api.interceptors.request.use(async (config) => {
-  const user = auth.currentUser;
+api.interceptors.request.use(
+  async (config) => {
+    const protectedRoutes = ['/bookings', '/users'];
 
-  if (user) {
-    const token = await user.getIdToken();
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    const isProtected = protectedRoutes.some(route =>
+      config.url.startsWith(route)
+    );
 
-  return config;
-});
+    if (isProtected) {
+      const user = auth.currentUser;
+      if (user) {
+        const token = await user.getIdToken();
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
